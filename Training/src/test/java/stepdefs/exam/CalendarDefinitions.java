1478a633fec48aee9284ca5af45fa2afd57a2bf6
“Hung Nguyen 3” <hung.nguyen3@asiantech.vn>
2020-10-20 10:27:19 +0700

    Update Definitions

--- Training/src/test/java/stepdefs/exam/CalendarDefinitions.java
+++ Training/src/test/java/stepdefs/exam/CalendarDefinitions.java
@@ -4,10 +4,13 @@ import at.base.BaseDefinitions;
 import at.core.PageFactory;
 import cucumber.api.java8.En;
 import page.exam.at_hungnguyen3.CalendarPage;
+import page.exam.at_hungnguyen3.CalendarPageAndroid;
+import page.exam.at_hungnguyen3.CalendarPageIOS;
 
 public class CalendarDefinitions extends BaseDefinitions implements En {
     CalendarPage calendarPage = new PageFactory<>(CalendarPage.class).create();
-
+    CalendarPageIOS calendarPageIOS = new PageFactory<>(CalendarPageIOS.class).create();
+    CalendarPageAndroid calendarPageAndroid = new PageFactory<>(CalendarPageAndroid.class).create();
     public CalendarDefinitions() {
 
         Given("^Calendar screen is opened$", () -> {calendarPage.open();
@@ -17,6 +20,12 @@ public class CalendarDefinitions extends BaseDefinitions implements En {
         When("^I check display initial time$", () -> {calendarPage.getTime(); });
         When("^I check display of Calendar screen$", () -> {calendarPage.isHistoryDisplay(); });
         Then("^Calendar is displayed$", () -> {calendarPage.isHistoryDisplay(); });
+        When("^I tap on info button$", () -> {calendarPageIOS.clickInfoButton(); });
+        When("^I tap on settings button$", () -> {calendarPageIOS.clickSettingsButton(); });
+        When("^I tap on daily dozen button$", () -> {calendarPageIOS.clickDailyDozenButton();
+        });
+        When("^I tap on next button$", () -> {calendarPageAndroid.clickToNextButton(); });
+        When("^I tap on next previous button$", () -> {calendarPageAndroid.clickToPreviousButton(); });
 
 
     }
