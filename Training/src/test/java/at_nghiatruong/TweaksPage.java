30c19ef715a530c20ca89b3602dc1e9a82fdd5ee
at-nghiatruong <nghia.truong@asiantech.vn>
2020-10-26 15:42:36 +0700

    Update fix comment

--- Training/src/test/java/at_nghiatruong/TweaksPage.java
+++ Training/src/test/java/at_nghiatruong/TweaksPage.java
@@ -28,24 +28,30 @@ public class TweaksPage extends BasePage {
 
     @AndroidFindBy(id= "tweak_name")
     @iOSFindBy(className = "UIAStaticText")
-    private List<MobileElement> tweakName;
+    private MobileElement tweakName;
 
     @AndroidFindBy(className = "android.widget.CheckBox")
     @iOSFindBy(className = "UIAButton")
-    List<MobileElement> listCheckboxes;
+    private MobileElement checkboxes;
 
     @AndroidFindBy(id = "tweak_history")
     @iOSFindBy(id = "ic calendar")
-    List<MobileElement> listHistory;
+    private MobileElement btnHistory;
 
     @AndroidFindBy(className = "android.widget.TextView")
     @iOSFindBy(id = "21 Tweaks")
     private MobileElement txtTitle;
 
     @AndroidFindBy(id = "tweak_icon")
+    private MobileElement TweakIcon;
+
+    @AndroidFindBy(id = "date_tweaks")
     @iOSFindBy(className = "UIAView")
-    List<MobileElement> listTweakIcon;
-    List<MobileElement> iosTweakContainer;
+    private MobileElement iOSContainer;
+    private MobileElement container;
+
+    @AndroidFindBy(className = "android.widget.LinearLayout")
+    List<MobileElement> listLayout;
 
     @Override
     public boolean isPageDisplayed() {
@@ -53,7 +59,7 @@ public class TweaksPage extends BasePage {
     }
 
     @Override
-    public BasePage open() {
+    public TweaksPage open() {
         if (!isPageDisplayed()) {
             getDriver().launchApp();
             homePage.clickMenuButton();
@@ -61,35 +67,37 @@ public class TweaksPage extends BasePage {
         return this;
     }
 
-    MobileElement dateTweaks = (MobileElement) getDriver().findElement(By.id("date_tweaks"));
-    List<MobileElement> container = dateTweaks.findElements(By.className("android.widget.LinearLayout"));
-
     public TweaksPage clickAllCheckboxes(){
-        for (int i = 0; i< container.size(); i++)
-            container.get(i).findElement(By.className("android.widget.CheckBox")).click();
-            scrollToElementContainText("Trendelenburg",container.get(20));
+        for (int i = 0; i< listLayout.size(); i++)
+            listLayout.get(i).findElement(By.className("android.widget.CheckBox")).click();
+            scrollToElementContainText("Trendelenburg",listLayout.get(20));
         return this;
     }
 
     public TweaksPage isAllCheckboxesSelected(){
-        for (int i = 0; i< container.size(); i++)
-            Assert.assertTrue(container.get(i).findElement(By.className("android.widget.CheckBox")).isSelected());
+        for (int i = 0; i< listLayout.size(); i++)
+            Assert.assertTrue(listLayout.get(i).findElement(By.className("android.widget.CheckBox")).isSelected());
         return this;
     }
 
     public TweaksPage getTitleMenuInTweaksList(){
-        container.get(pos).findElement(By.id("tweak_name")).getText();
+        listLayout.get(pos).findElement(By.id("tweak_name")).getText();
         return this;
     }
 
     int pos;
-    public TweaksPage clickCheckboxInTweaksList(){
-        container.get(pos).findElement(By.className("android.widget.CheckBox")).click();
+    public TweaksPage clickCheckboxInListLayout(){
+        listLayout.get(pos).findElement(By.className("android.widget.CheckBox")).click();
         return this;
     }
 
     public String getTextTitle() {
         waitForElementDisplay(txtTitle);
         return txtTitle.getText();
+    }
+
+    public TweaksPage clickOnHistoryButton(){
+        waitForElementDisplay(btnHistory);
+        btnHistory.click();
     }
 }
