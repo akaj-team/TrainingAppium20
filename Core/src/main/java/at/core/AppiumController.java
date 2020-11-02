70d7ff57d708a93023550278d445a01ac2a3807b
at_cuongnguyen <cuong.nguyen@asiantech.vn>
2020-11-02 14:16:51 +0700

    fix confliction

--- Core/src/main/java/at/core/AppiumController.java
+++ Core/src/main/java/at/core/AppiumController.java
@@ -109,8 +109,8 @@ class AppiumController {
     private Map<String, String> defaultAndroidParameters() {
         Map<String, String> parameters = new HashMap<>();
         parameters.put(MobileCapabilityType.PLATFORM_NAME, "android");
-        parameters.put(MobileCapabilityType.DEVICE_NAME, "Pixel API 28 2");
-        parameters.put(MobileCapabilityType.PLATFORM_VERSION, "9.0");
+        parameters.put(MobileCapabilityType.DEVICE_NAME, "Pixel XL API 27");
+        parameters.put(MobileCapabilityType.PLATFORM_VERSION, "8.1");
         parameters.put(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
         parameters.put(AndroidMobileCapabilityType.APP_PACKAGE, "org.nutritionfacts.dailydozen");
         parameters.put(MobileCapabilityType.APP, "org.nutritionfacts.dailydozen.apk");
@@ -121,7 +121,7 @@ class AppiumController {
     private Map<String, String> defaultIosParameters() {
         Map<String, String> parameters = new HashMap<>();
         parameters.put(MobileCapabilityType.PLATFORM_NAME, "ios");
-        parameters.put(MobileCapabilityType.DEVICE_NAME, "iPhone 8");
+        parameters.put(MobileCapabilityType.DEVICE_NAME, "iPhone 11");
         parameters.put(MobileCapabilityType.PLATFORM_VERSION, "13.7");
         parameters.put(MobileCapabilityType.AUTOMATION_NAME, "XCUITest");
         parameters.put(MobileCapabilityType.APP, "org.nutritionfacts.dailydozen.app");
